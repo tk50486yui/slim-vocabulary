da15df8e718b7098ed242911bfb0fcbdb9e3366a
Yue <tk50486yui@gmail.com>
2023-08-02 15:03:57 +0800

    categories column

--- api/app/Models/Categories.php
+++ api/app/Models/Categories.php
@@ -118,6 +118,9 @@ class Categories
                 $node = array(
                     'id' => $category['id'],
                     'cate_name' => $category['cate_name'],
+                    'cate_parent_id' => $category['cate_parent_id'],
+                    'cate_level' => $category['cate_level'],
+                    'cate_order' => $category['cate_order'],
                     'parents' => $parents,
                     'children' => $this->buildCategoriesTree($categories, $category['id'], array_merge($parents, [$category['id']]))
                 );              
