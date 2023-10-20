1632ca5ba58c45954d71d2379745cc6038d78257
Yue <tk50486yui@gmail.com>
2023-10-20 11:26:04 +0800

    set .env

--- api/core/Model.php
+++ api/core/Model.php
@@ -3,18 +3,25 @@
 namespace core;
 
 use \RedBeanPHP\R as R;
-use \core\Config;
+use Dotenv\Dotenv;
 
 class Model
 {
     public static function begin()
     { 
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+        $dbHost = $_ENV['DB_HOST'];
+        $dbPort = $_ENV['DB_PORT'];
+        $dbName = $_ENV['DB_NAME'];
+        $dbUser = $_ENV['DB_USER'];
+        $dbPassword = $_ENV['DB_PASSWORD'];
         R::setup(
-            'pgsql:host=' . Config::DB_HOST . 
-            ';port='.Config::DB_PORT.
-            ';dbname=' . Config::DB_NAME,
-            Config::DB_USER,
-            Config::DB_PASSWORD
+            'pgsql:host=' . $dbHost . 
+            ';port='.$dbPort.
+            ';dbname=' .$dbName,
+            $dbUser,
+            $dbPassword
         );
         R::freeze(true); 
         
